110e2ee204a6695629c424c3aee013dca3800867
Thomas Holmes <thomas@devminded.com>
2013-03-10 18:19:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ginger::Application.config.secret_token = '9320f83302eb7c04df030c43c33baedb687f77f2130c939a24c34f2bb68f5dd9cefda6ef9223bb0c6810933ea381fe1fa01dd9ff113cc37d9fcde6da871c6d74'
